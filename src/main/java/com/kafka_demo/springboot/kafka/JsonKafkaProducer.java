1f5463c3798923abeea12100e94db1036c70787c
Swapnil <swapnil.sarkar99@gmail.com>
2024-11-30 11:04:02 +0530

    first commit

---
+++ src/main/java/com/kafka_demo/springboot/kafka/JsonKafkaProducer.java
@@ -0,0 +1,35 @@
+package com.kafka_demo.springboot.kafka;
+
+import com.kafka_demo.springboot.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    // whenever there is only one parameterized constructor, springboot framework automatically inject this dependency
+    public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(User data) {
+
+        LOGGER.info(String.format("Message sent -> %s", data.toString()));
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "myJsonTopic")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
